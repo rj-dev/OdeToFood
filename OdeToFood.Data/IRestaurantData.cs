a6229fe03175d124201a90ec2a252553f2277e72
Renato Silva <rjsilva@outlook.com>
2019-05-25 13:38:32 +0100

    new init

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() {
+                new Restaurant{ Id=1, Name="Scott's Pizza",Location="Maryland",Cuisine=CuisineType.Italain},
+                new Restaurant{ Id=2, Name="Cinnamon Club",Location="London",Cuisine=CuisineType.Mexican},
+                new Restaurant{ Id=3, Name="La Costa",Location="California",Cuisine=CuisineType.Indian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
